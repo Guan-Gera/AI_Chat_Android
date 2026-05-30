9c327051df72961ccb438ea1c6f52d4fe27384d5
Guan-Gera <122550415+Guan-Gera@users.noreply.github.com>
2026-05-30 14:02:26 +0800

    Delete app directory

--- app/src/main/java/com/aichat/app/network/AiStreamEvent.kt
+++
@@ -1,7 +0,0 @@
-package com.aichat.app.network
-
-sealed interface AiStreamEvent {
-    data object Started : AiStreamEvent
-    data class Delta(val text: String) : AiStreamEvent
-    data object Finished : AiStreamEvent
-}
